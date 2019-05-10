c0a7bb78f24f568e47658f132b87ae423efcbd5b
RobotShop <cbenson@robotshop.com>
2019-05-10 11:13:59 -0400

    millis & settings
    Added a reset to the Millis 
    Added other parameters to smooth the servo reaction

--- LSS/examples/LSS_Knob_Mimic/LSS_Knob_Mimic.ino
+++ LSS/examples/LSS_Knob_Mimic/LSS_Knob_Mimic.ino
@@ -1,14 +1,14 @@
 /*
- *	Author:			Sebastien Parent-Charette (support@robotshop.com)
- *	Version:		1.0.0
- *	Licence:		LGPL-3.0 (GNU Lesser General Public License version 3)
- *	
- *	Desscription:	Moves one LSS using the position of a second LSS.
+ *  Author:     Sebastien Parent-Charette (support@robotshop.com)
+ *  Version:    1.0.0
+ *  Licence:    LGPL-3.0 (GNU Lesser General Public License version 3)
+ *  
+ *  Desscription: Moves one LSS using the position of a second LSS.
 */
 
 #include <LSS.h>
 
-#define LSS_BAUD	(LSS_DefaultBaud)
+#define LSS_BAUD  (LSS_DefaultBaud)
 
 // Create two LSS object; one for output (ID=0), one for input (ID=1)
 LSS myLSS_Output = LSS(0);
@@ -16,21 +16,23 @@ LSS myLSS_Input = LSS(1);
 
 void setup()
 {
-	// Initialize the LSS bus
-	LSS::initBus(Serial, LSS_BAUD);
-	
-	// Initialize LSS output to position 0.0
-	myLSS_Output.move(0);
-	
-	// Wait for it to get there
-	delay(2000);
-	
-	// Lower output servo stiffness
-	myLSS_Output.setAngularStiffness(4);
-	myLSS_Output.setMaxSpeedRPM(15);
-	
-	// Make input servo limp (no active resistance)
-	myLSS_Input.limp();
+  // Initialize the LSS bus
+  LSS::initBus(Serial, LSS_BAUD);
+  
+  // Initialize LSS output to position 0.0
+  myLSS_Output.move(0);
+  
+  // Wait for it to get there
+  delay(2000);
+  
+  // Lower output servo stiffness & accelerations
+  myLSS_Output.setAngularStiffness(0);
+  myLSS_Output.setAngularHoldingStiffness(0);
+  myLSS_Output.setAngularAcceleration(15);
+  myLSS_Output.setAngularDeceleration(15);
+  
+  // Make input servo limp (no active resistance)
+  myLSS_Input.limp();
 }
 
 // Reproduces position of myLSS_Input on myLSS_Output
@@ -38,14 +40,15 @@ unsigned long startTime = millis();
 
 void loop()
 {
-	// Wait ~20 ms before sending another command (update at most 50 times per second)
-	if((millis() - startTime) > 20)
-	{
-		int16_t pos = (int16_t)myLSS_Input.getPosition();
-		Serial.print("Input @ "); Serial.println(pos);
-		
-		// Send LSS to half a turn counter-clockwise from zero (assumes gyre = 1)
-		myLSS_Output.move(pos);
-		Serial.println("\r\n");
-	}
+  // Wait ~25 ms before sending another command (update at most 25 times per second)
+  if((millis() - startTime) > 25)
+  {
+    startTime = millis();
+    int16_t pos = (int16_t)myLSS_Input.getPosition();
+    Serial.print("Input @ "); Serial.println(pos);
+    
+    // Send LSS to half a turn counter-clockwise from zero (assumes gyre = 1)
+    myLSS_Output.move(pos);
+    Serial.println("\r\n");
+  }
 }
