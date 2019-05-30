b97600df9ba259c18c1c9647b130d9e182e0c940
rbbdaouas <bdaouas@robotshop.com>
2019-05-30 10:01:04 -0400

    New example for wheel mode

---
+++ LSS/examples/LSS_Wheel/LSS_Wheel.ino
@@ -0,0 +1,33 @@
+/*
+ *	Author:			Brahim Daouas (support@robotshop.com)
+ *	Version:		1.0.0
+ *	Licence:		LGPL-3.0 (GNU Lesser General Public License version 3)
+ *	
+ *	Desscription:	Basic example of the LSS rotating continuously.
+*/
+
+#include <LSS.h>
+
+// ID set to default LSS ID = 0
+#define LSS_ID		(0)
+#define LSS_BAUD	(LSS_DefaultBaud)
+
+// Create one LSS object
+LSS myLSS = LSS(LSS_ID);
+
+void setup()
+{
+	// Initialize the LSS bus
+	LSS::initBus(Serial, LSS_BAUD);
+
+  // wait LSS to boot
+  delay(2000);
+	// move LSS continuously
+	myLSS.wheelRPM(100);
+
+}
+
+void loop()
+{
+	// Nothing to do in loop
+}
